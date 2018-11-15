532d4ba8543120bf7159ae063383a565a0dd6463
Divya Mahendia <38568931+DivyaMahendia@users.noreply.github.com>
2018-11-16 01:54:16 +0530

    EuclidAlgo

--- GCDEuclid.cpp
+++ GCDEuclid.cpp
@@ -1,3 +1,5 @@
+// Program to to find GCD of two numbers using Euclidean algorithm.
+
 #include<iostream>
 #include<conio.h>
 #include<stdlib.h>
@@ -8,16 +10,15 @@ int gcd(int u, int v)
     return (v != 0) ? gcd(v, u % v) : u;
 }
 
-int main(void)
+int main()
 {
     int num1, num2, result;
-    cout << "Enter two numbers to find GCD using Euclidean algorithm: ";
-    cin >> num1 >> num2;
+    cout<< "Enter two numbers to find GCD using Euclidean algorithm: ";
+    cin>>num1>>num2;
     result = gcd(num1, num2);
-    if (gcd)
-        cout << "\nThe GCD of " << num1 << " and " << num2 << " is: " << result
-                << endl;
+    if(gcd)
+        cout<<"\nThe GCD of "<<num1<<" and "<<num2<<" is: "<< result<< endl;
     else
-        cout << "\nInvalid input!!!\n";
+        cout<<"\nInvalid input!!!\n";
     return 0;
 }
