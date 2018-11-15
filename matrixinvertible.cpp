401f0153e40381705a476c0fcf413193a0fd32c3
Divya Mahendia <38568931+DivyaMahendia@users.noreply.github.com>
2018-11-16 02:15:36 +0530

    done

--- matrixinvertible.cpp
+++ matrixinvertible.cpp
@@ -6,24 +6,24 @@
 #include<math.h>
 
 using namespace std;
-double d = 0;
+double d=0;
 double det(int n, double mat[10][10]);
 double det(int n, double mat[10][10])
 {
     double submat[10][10];
-    if (n == 2)
-        return ((mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]));
+    if(n == 2)
+        return((mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]));
     else
     {
-        for (int c = 0; c < n; c++)
+        for(int c = 0; c < n; c++)
         {
             int subi = 0; //submatrix's i value
-            for (int i = 1; i < n; i++)
+            for(int i = 1; i < n; i++)
             {
                 int subj = 0;
-                for (int j = 0; j < n; j++)
+                for(int j = 0; j < n; j++)
                 {
-                    if (j == c)
+                    if(j == c)
                         continue;
                     submat[subi][subj] = mat[i][j];
                     subj++;
@@ -39,24 +39,24 @@ double det(int n, double mat[10][10])
 int main(int argc, char **argv)
 {
 
-    cout << "Enter the dimension of the matrix:\n";
+    cout<<"Enter the dimension of the matrix:\n";
     int n;
-    cin >> n;
+    cin>>n;
     double mat[10][10];
-    cout << "Enter the elements of the matrix:\n";
-    for (int i = 0; i < n; i++)
+    cout<<"Enter the elements of the matrix:\n";
+    for(int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for(int j = 0; j < n; j++)
         {
-            cin >> mat[j][i];
+            cin>>mat[j][i];
         }
     }
-    if (det(n, mat) != 0)
+    if(det(n, mat) != 0)
     {
-        cout << "The given matrix is invertible";
+        cout<<"The given matrix is invertible";
     }
     else
     {
-        cout << "The given matrix is not invertible";
+        cout<<"The given matrix is not invertible";
     }
 }
